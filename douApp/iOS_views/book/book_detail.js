96d241edd7b1392985fecba7a8091d10705aaa81
easyui <neu@neudeMacBook-Air.local>
2016-04-15 12:25:17 +0800

    固定Header

--- douApp/iOS_views/book/book_detail.js
+++ douApp/iOS_views/book/book_detail.js
@@ -22,31 +22,34 @@ module.exports = React.createClass({
   },
   render: function(){
     return(
-      <ScrollView style={styles.m10}>
-        {
-          this.state.data ?
-            <View>
-              <Header
+        <View style={[styles.m10]}>
+            <Header
                 navigator={this.props.navigator}
                 initObj={{
                     backName: '图书',
-                    title: this.state.data.title
+                    title: this.state.data?this.state.data.title:'',
                 }}/>
-              <BookItem row={this.state.data}/>
-              <View>
-                <Text style={[styles.title]}>图书简介</Text>
-                <Text style={styles.text}>{this.state.data.summary}</Text>
-              </View>
+          <ScrollView style={styles.m10} >
+            {
+              this.state.data ?
+                  <View>
+                    <BookItem row={this.state.data}/>
+                    <View>
+                      <Text style={[styles.title]}>图书简介</Text>
+                      <Text style={styles.text}>{this.state.data.summary}</Text>
+                    </View>
 
-              <View>
-                <Text style={[styles.title]}>作者简介</Text>
-                <Text style={styles.text}>{this.state.data.author_intro}</Text>
-              </View>
-              <View style={{height:50}}></View>
-            </View>
-            : Util.loading
-        }
-      </ScrollView>
+                    <View>
+                      <Text style={[styles.title]}>作者简介</Text>
+                      <Text style={styles.text}>{this.state.data.author_intro}</Text>
+                    </View>
+                    <View style={{height:50}}></View>
+                  </View>
+                  : Util.loading
+            }
+          </ScrollView>
+        </View>
+
     );
   },
 
@@ -66,7 +69,8 @@ module.exports = React.createClass({
 
 var styles = StyleSheet.create({
   m10:{
-    flex:1
+    flex:1,
+    top:0,
   },
   title:{
     fontSize:16,
